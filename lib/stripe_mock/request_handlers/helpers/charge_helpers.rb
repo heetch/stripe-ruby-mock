29a0406580f10d8dddf7359469e1fe2b6d4d0e7d
Nathan Appere <nathan.appere@gmail.com>
2022-12-21 11:34:26 +0100

    Fix refund charge helper exception

--- lib/stripe_mock/request_handlers/helpers/charge_helpers.rb
+++ lib/stripe_mock/request_handlers/helpers/charge_helpers.rb
@@ -1,14 +1,26 @@
+# NOTE: somehow we receive an array instead of the new expected format.
 module StripeMock
   module RequestHandlers
     module Helpers
 
       def add_refund_to_charge(refund, charge)
         refunds = charge[:refunds]
+
+        if refunds.is_a?(Array)
+          refunds = { data: refunds }
+        elsif refunds.is_a?(Hash)
+          refunds[:data] = [] if !refunds[:data]
+        else
+          refunds = { data: [] }
+        end
+
         refunds[:data] << refund
         refunds[:total_count] = refunds[:data].count
 
         charge[:amount_refunded] = refunds[:data].reduce(0) {|sum, r| sum + r[:amount].to_i }
         charge[:refunded] = true
+
+        charge[:refunds] = refunds
       end
 
     end
